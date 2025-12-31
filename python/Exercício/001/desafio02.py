183dc63c0ffb219285f1daec5382574e6aa41b09
Passoz <pedronascimentopassoz@gmail.com>
2025-12-31 20:55:44 -0300

    ex novos

---
+++ python/Exercício/001/desafio02.py
@@ -0,0 +1,6 @@
+print('Desafio 2')
+dia = input('Dia de nascimento: ')
+mês = input('Mês de nascimento: ')
+ano = input('Ano de nascimento: ')
+
+print('Você nasceu no Dia', dia + '/' + mês + '/' + ano + '!')
